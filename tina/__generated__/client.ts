91b605125c695b7b1939b9fd2ba417eb9803d82b
Elliot Forde <elliot@Elliots-MacBook-Air.local>
2026-06-14 17:41:43 +0100

    Regenerate Tina schema after image field revert

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ cacheDir: '/Users/elliot/mcr-eats/tina/__generated__/.cache/1781454028181', url: 'https://content.tinajs.io/2.4/content/a01b4841-b4cf-4dc6-833b-32bd07c92e4a/github/main', token: '11be4272ef9c96302cf791b799095ac255307325', queries,  });
+export const client = createClient({ cacheDir: '/Users/elliot/mcr-eats/tina/__generated__/.cache/1781455285722', url: 'https://content.tinajs.io/2.4/content/a01b4841-b4cf-4dc6-833b-32bd07c92e4a/github/main', token: '11be4272ef9c96302cf791b799095ac255307325', queries,  });
 export default client;
   
